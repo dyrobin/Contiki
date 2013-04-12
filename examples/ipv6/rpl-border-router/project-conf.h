d115b80da0c4ed0d07b26f62f8622b6965c71029
Yang <yang.deng@aalto.fi>
2013-04-12 16:25:50 +0300

    add different domain support

--- examples/ipv6/rpl-border-router/project-conf.h
+++ examples/ipv6/rpl-border-router/project-conf.h
@@ -51,4 +51,23 @@
 #define WEBSERVER_CONF_CFS_CONNS 2
 #endif
 
+#undef 	PMPD_ENABLED
+#define PMPD_ENABLED    1
+
+#if PMPD_ENABLED == 0
+#undef 	UIP_CONF_BUFFER_SIZE
+#define UIP_CONF_BUFFER_SIZE 140
+#endif
+
+#undef 	DIFF_DOMAIN
+#define DIFF_DOMAIN   1
+
+#undef 	DIFF_DOMAIN_RECEIVER
+#define DIFF_DOMAIN_RECEIVER 	0
+
+#if DIFF_DOMAIN == 1 && DIFF_DOMAIN_RECEIVER == 1
+#define SICSLOWPAN_CONF_ADDR_CONTEXT_0 {addr_contexts[0].prefix[0]=0xbb;addr_contexts[0].prefix[1]=0xbb;}
+#endif
+
+
 #endif /* __PROJECT_ROUTER_CONF_H__ */
--- examples/prediction-test/project-conf.h
+++ examples/prediction-test/project-conf.h
@@ -1,15 +1,22 @@
 #ifndef __PROJECT_PMPD_TEST_CONF_H__
 #define __PROJECT_PMPD_TEST_CONF_H__
 
-#undef PMPD_ENABLED
+#undef 	PMPD_ENABLED
 #define PMPD_ENABLED    1
 
 #if PMPD_ENABLED == 0
-#undef UIP_CONF_BUFFER_SIZE
+#undef 	UIP_CONF_BUFFER_SIZE
 #define UIP_CONF_BUFFER_SIZE 140
 #endif
 
-#undef DIFF_DOMAIN
-//#define DIFF_DOMAIN   1
+#undef 	DIFF_DOMAIN
+#define DIFF_DOMAIN   1
+
+#undef 	DIFF_DOMAIN_RECEIVER
+#define DIFF_DOMAIN_RECEIVER 	0
+
+#if DIFF_DOMAIN == 1 && DIFF_DOMAIN_RECEIVER == 1
+#define SICSLOWPAN_CONF_ADDR_CONTEXT_0 {addr_contexts[0].prefix[0]=0xbb;addr_contexts[0].prefix[1]=0xbb;}
+#endif
 
 #endif /* __PROJECT_PMPD_TEST_CONF_H__ */
