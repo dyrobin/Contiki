8c14a02f7e41b754e1f9aa714e815295a0ba3710
Yang <yang.deng@aalto.fi>
2014-06-16 17:11:39 +0300

    change run.sh

--- expt/run.sh
+++ expt/run.sh
@@ -1,134 +1,135 @@
 #! /bin/sh
-
-# handle arguments
-if [ $# -ne 2 ] && [ $# -ne 3 ]; then
-    echo "Usage: `basename $0` [-d] INTVL(s) RX(%)"
-    exit 1
-fi
-
-if [ $# -eq 3 ]; then
-    if [ $1 != "-d" ]; then
-        echo "Usage: `basename $0` [-d] INTVL(s) RX(%)"
-        exit 1
-    else
-        DAEnabled="true"
-        shift
-    fi
-fi
-
-if echo $1 | egrep -q "^([0-9]|[1-9][0-9]|100)$"; then
-    TFCINTVL=$1
-else
-    echo "Invalid INTVL: it must be an integer within [0, 100]"
-    exit 1
-fi
-
-if echo $2 | egrep -q "^([1-9]|[1-9][0-9]|100)$"; then
-    RXPCT=$2
-    RXRATIO=`echo ${RXPCT} | awk '{printf("%.2f", $1 / 100)}'`
-else
-    echo "Invalid RX: it must be an integer within [1, 100]"
-    exit 1
-fi
+#
+# Run experiment where variables can be configured by options.
+# Authored by Yang Deng <yang.deng@aalto.fi>
 
 
-# run experiments for different datasize, 
-# ${TFCINTVL}, ${RXPCT}, ${TPDUSIZE} must be defined before calling this
-run_diff_data() {
-    # datasize starts from 1K
-    local datasize=512
-
-    for j in `seq 1 5`; do
-        datasize=$((${datasize} * 2))
-        prefix="${TFCINTVL}_${RXPCT}_${TPDUSIZE}_${datasize}"
-        cscfile=${prefix}_${SUFFIX}.csc
-        jsfile=${prefix}_${SUFFIX}.js
-        logfile=${prefix}_${SUFFIX}.log
-
-        #generate new js file according to the template
-        sed -e "s/\(tfc_intvl = \).*/\1${TFCINTVL};/" \
-            -e "s/\(rx_pct = \).*/\1${RXPCT};/" \
-            -e "s/\(tpdu_size = \).*/\1${TPDUSIZE};/" \
-            -e "s/\(data_size = \).*/\1${datasize};/" \
-            ${JSTEMPLATE} > ${SIMDIR}/${jsfile}
-        
-        #generate new csc file according to the template
-        sed -e "s/\(<success_ratio_rx>\).*\(<\/success_ratio_rx>\)/\1${RXRATIO}\2/" \
-            -e "s/\(<scriptfile>.*\/\)[^\/]*\(<\/scriptfile>\)/\1${jsfile}\2/" \
-            ${CSCTEMPLATE} > ${SIMDIR}/${cscfile}
-
-#       # code to support concurrent jobs
-#       jobnum=`ps --no-headers -C java | wc -l`
-#       while [ ${jobnum} -ge ${CPUNUM} ]; do
-#           sleep 10
-#           jobnum=`ps --no-headers -C java | wc -l`
-#       done
-
-        # Not sure if cooja have concurrent problems or not, so we run the tests
-        # one by one
-        STIME=`date +%s`
-        echo -n "Running ${prefix} ... "
-        java -mx512m -jar ${CONTIKIDIR}/tools/cooja/dist/cooja.jar \
-             -nogui=${SIMDIR}/${cscfile} -contiki=${CONTIKIDIR} 1>/dev/null
-
-        if [ -f "COOJA.testlog" ] && grep -q "TEST OK" COOJA.testlog; then
-            echo -n "OK. "
-            mv COOJA.testlog ${LOGDIR}/${logfile}
-        else
-            echo -n "Failed. "
-            if [ -f "COOJA.testlog" ]; then
-                mv COOJA.testlog ${LOGDIR}/${logfile}
-            fi
-
-            if [ -f "COOJA.log" ]; then
-                echo "-- OUTPUT LOG --" >> ${LOGDIR}/${logfile}
-                tail -20 COOJA.log >> ${LOGDIR}/${logfile}
-            fi
-        fi
-
-        rm -f COOJA.log
-        ETIME=`date +%s`
-        echo "Duration: `date -u -d @$((${ETIME} - ${STIME})) +%H:%M:%S`"
-    done
-    #wait // concurrent support
+print_usage() {
+    echo "Usage: $0 [-c contiki_dir] [-i traffic_interval]"
+    echo "       [-r success_ratio_rx] [-t tpdu_size]"
+    echo "       [-s data_size] output_dir"
 }
 
+print_invalid_opt() {
+    echo "Invalid \"-${flag}\": \"${OPTARG}\" $1"
+}
 
-#    # Max jobs running concurrently
-#    CPUNUM=`cat /proc/cpuinfo | grep processor | wc -l`
+# default config values
+contikidir=`cd .. && pwd`
 
+tfcintvl=0
+rxratio=100
+tpdusize=65
+datasize=1024
 
-# create dirs for saving simulation configs and log outputs
-SIMDIR=sims
-if [ ! -d ${SIMDIR} ]; then
-    mkdir ${SIMDIR}
+# get opts to change config values
+while getopts c:i:r:t:s: flag; do
+    case ${flag} in
+        c)  if [ -d ${OPTARG} ] && 
+               echo `cd ${OPTARG} && pwd` | grep -q "contiki$"; then
+                contikidir=`cd ${OPTARG} && pwd`
+            else
+                print_invalid_opt "is not a contiki directory"
+                exit 1
+            fi
+            ;;
+        i)  if echo ${OPTARG} | egrep -q "^([0-9]|[1-5][0-9]|60)$"; then
+                tfcintvl=${OPTARG}
+            else
+                print_invalid_opt "is not an integer within [0, 60]"
+                exit 1
+            fi
+            ;;
+        r)  if echo ${OPTARG} | egrep -q "^([1-9]|[1-9][0-9]|100)$"; then
+                rxratio=${OPTARG}               
+            else
+                print_invalid_opt "is not an integer within [1, 100]"
+                exit 1
+            fi
+            ;;
+        t)  if echo ${OPTARG} | egrep -q "^(0|[1-9][0-9]*)$"; then
+                tpdusize=${OPTARG}
+            else
+                print_invalid_opt "is not an integer"
+                exit 1
+            fi
+            ;;
+        s)  if echo ${OPTARG} | egrep -q "^(0|[1-9][0-9]*)$"; then
+                datasize=${OPTARG}
+            else
+                print_invalid_opt "is not an integer"
+                exit 1
+            fi
+            ;;
+        ?)  print_usage
+            exit 1
+            ;;
+    esac
+done
+shift $((${OPTIND} - 1));
+
+if [ $# -ne 1 ]; then
+    print_usage
+    exit 1
 fi
 
-LOGDIR=logs
-if [ ! -d ${LOGDIR} ]; then
-    mkdir ${LOGDIR}
+mkdir -p $1
+if [ $? -ne 0 ]; then
+    exit 1
 fi
 
-# several configs
-CONTIKIDIR=~/workspace/contiki
+# main part starts here
+prefix="${tfcintvl}_${rxratio}_${tpdusize}_${datasize}"
+suffix=sim15
 
-SUFFIX=sim15
-CSCTEMPLATE=csc_template_${SUFFIX}
-JSTEMPLATE=js_template_${SUFFIX}
+csctemplate=csc_template_${suffix}
+jstemplate=js_template_${suffix}
+if [ ! -f ${csctemplate} ] || [ ! -f ${jstemplate} ]; then
+    echo "Error: ${csctemplate} or ${jstemplate} does not exist."
+    exit 1
+fi
+
+outputdir=`cd $1 && pwd`
+cscfile=${prefix}_${suffix}.csc
+jsfile=${prefix}_${suffix}.js
+logfile=${prefix}_${suffix}.log
+
+# generate new js file from jstemplate; config values are rewritten here
+sed -e "s/\(tfc_intvl = \).*/\1${tfcintvl};/" \
+    -e "s/\(rx_ratio = \).*/\1${rxratio};/" \
+    -e "s/\(tpdu_size = \).*/\1${tpdusize};/" \
+    -e "s/\(data_size = \).*/\1${datasize};/" \
+    ${jstemplate} > ${outputdir}/${jsfile}
+
+# generate new csc file from csctemplate; config values are rewritten here
+rxratio_f=`echo ${rxratio} | awk '{printf("%.2f", $1 / 100)}'`
+sed -e "s/\(<success_ratio_rx>\).*\(<\/success_ratio_rx>\)/\1${rxratio_f}\2/" \
+    -e "s/\(<scriptfile>.*\/\)[^\/]*\(<\/scriptfile>\)/\1${jsfile}\2/" \
+    ${csctemplate} > ${outputdir}/${cscfile}
 
 
-# do the tests now
-STTIME=`date +%s`
-if [ ! -z ${DAEnabled} ] && [ ${DAEnabled} = "true" ]; then
-    TPDUSIZE=0
-    run_diff_data
+# run test
+STIME=`date +%s`
+echo -n "Running ${prefix} ... "
+
+java -mx1536m -jar ${contikidir}/tools/cooja/dist/cooja.jar \
+     -nogui=${outputdir}/${cscfile} -contiki=${contikidir} 1>/dev/null
+
+if [ -f "COOJA.testlog" ] && grep -q "TEST OK" COOJA.testlog; then
+    echo -n "OK. "
+    mv COOJA.testlog ${outputdir}/${logfile}
 else
-    TPDUSIZE=0
-    TPDUSTEP=65
-    for i in `seq 1 6`; do
-        TPDUSIZE=$((${TPDUSIZE} + ${TPDUSTEP}))
-        run_diff_data
-    done
+    echo -n "Failed. "
+    if [ -f "COOJA.testlog" ]; then
+        mv COOJA.testlog ${outputdir}/${logfile}
+    fi
+
+    if [ -f "COOJA.log" ]; then
+        echo "-- OUTPUT LOG --" >> ${outputdir}/${logfile}
+        tail -20 COOJA.log >> ${outputdir}/${logfile}
+    fi
 fi
-ETTIME=`date +%s`
-echo "Done. Total Time: `date -u -d @$((${ETTIME} - ${STTIME})) +%H:%M:%S`"
+rm -f COOJA.log
+
+ETIME=`date +%s`
+echo "Duration: `date -u -d @$((${ETIME} - ${STIME})) +%H:%M:%S`"
---
+++ expt/runmany.sh
@@ -0,0 +1,22 @@
+#! /bin/sh
+#
+# Run multiple experiments; each experiment is done by run.sh
+# Authored by Yang Deng <yang.deng@aalto.fi>
+
+# change following variables to determine which experiments to be done.
+intvl_list="0 5 10"
+rx_list="100 85 70 55"
+tpdu_list=`seq 0 65 390`
+data_list="1024 2048 4096 8192 16384"
+
+# run multiple experiments
+for intvl in ${intvl_list}; do
+    for rx in ${rx_list}; do
+        for tpdu in ${tpdu_list} ; do
+            for data in ${data_list}; do
+                sh run.sh -i ${intvl} -r ${rx} -t ${tpdu} -s ${data} sims
+            done
+        done
+    done
+done
+
